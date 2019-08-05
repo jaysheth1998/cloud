9408d27d85007a1de9ef1144ab6ee08baff19dfe
Jay Sheth <JaySheth@Jays-MacBook-Pro.local>
2019-08-04 22:17:50 -0400

    Added cloud cuz ronak did the work

---
+++ precache-manifest.711b87aecf03b3cd007c650301710481.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "75a498ce6157e3ab29cc",
+    "url": "/static/css/main.54accdbd.chunk.css"
+  },
+  {
+    "revision": "75a498ce6157e3ab29cc",
+    "url": "/static/js/main.75a498ce.chunk.js"
+  },
+  {
+    "revision": "ea29b4527a7375faed1a",
+    "url": "/static/js/1.ea29b452.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1c04898c70a089ce777c4621e74975b3",
+    "url": "/index.html"
+  }
+];
